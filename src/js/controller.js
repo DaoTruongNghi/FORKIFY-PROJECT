4cb87e23a622039f42f7a2b9604a326fe17721fe
DaoTruongNghi <daotruongnghi0812@gmail.com>
2023-10-23 15:58:42 +0700

    New fearture adding

--- src/js/controller.js
+++ src/js/controller.js
@@ -141,6 +141,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the application");
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -149,5 +152,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
